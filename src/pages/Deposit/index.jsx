efd145f4f73d2983e41380e6a3eed3a90dd2af78
Khoa <khoaho120703@gmail.com>
2025-03-13 14:25:31 +0700

    Deposit

--- src/pages/Deposit/index.jsx
+++ src/pages/Deposit/index.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import InfoCheckout from "../../components/InfoCheckout";
 
 function Deposit() {
-  return <div>Deposit</div>;
+  const [amount, setAmount] = useState(0);
+  const [description, setDesciption] = useState("");
+  return (
+    <div className="my-5 grid w-full max-w-7xl grid-cols-3 justify-center gap-10">
+      <div className="col-span-2">
+        <InfoCheckout />
+      </div>
+      <div className="flex flex-col rounded-lg border border-violet-200 bg-[#F1E0FD] p-5 text-gray-600">
+        <h3 className="mb-5 text-2xl font-bold">Thông Tin Thanh Toán</h3>
+        <div className="mb-5 flex w-full flex-col gap-2">
+          <label className="font-semibold" htmlFor="amount">
+            Số Tiền Nạp (đ)
+          </label>
+          <input
+            type="number"
+            min={5000}
+            value={amount.toLocaleString("vi-VN")}
+            name="amount"
+            id="amount"
+            className="rounded-lg border border-violet-400 bg-transparent px-4 py-2 outline-none"
+            onChange={(e) => setAmount(e.target.value)}
+          />
+        </div>
+        <div className="mb-5 flex w-full flex-col gap-2">
+          <label className="flex items-center gap-2 font-semibold">
+            Mô Tả
+            <p className="text-xs">(Không bắt buộc)</p>
+          </label>
+          <textarea
+            value={description}
+            name="description"
+            id="desciption"
+            className="rounded-lg border border-violet-400 bg-transparent px-4 py-2 outline-none"
+            onChange={(e) => setDesciption(e.target.value)}
+            rows={5}
+          />
+        </div>
+
+        <button className="rounded-lg bg-heading px-5 py-2 text-white">
+          Thanh Toán
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Deposit;
--- src/pages/Premium/Buy.jsx
+++ src/pages/Premium/Buy.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import LoadingSpinner from "../../components/Loading/LoadingSpinner";
+import InfoCheckout from "../../components/InfoCheckout";
 
 function Buy() {
+  const person = {
+    name: "Nguyễn Thúc Thùy Tiên",
+    gender: "Female",
+    email: "tiencute@gmail.com",
+    phone: "0901232323",
+  };
   return (
-    <div>
-      <LoadingSpinner />
+    <div className="mt-5 w-full bg-red-400">
+      <InfoCheckout />
+      <div></div>
     </div>
   );
 }
